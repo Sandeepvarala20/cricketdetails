b36566ac1eb9faaf14dadc54614576112b14fd10
Sandeep Varala <Varalas5198@uhcl.edu>
2023-06-21 00:49:35 -0700

    Revert "updated with weather conditions"
    This reverts commit 5b107ad52dcc0e75a1f3ee5cdf7c2a997d60970e.

--- src/main/java/com/example/CricketAPI/cricketController.java
+++ src/main/java/com/example/CricketAPI/cricketController.java
@@ -1,15 +1,14 @@
 package com.example.CricketAPI;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
+
 public class cricketController extends cricketUtils{
 	
-	@GetMapping("/getdetails")
+	@GetMapping("/getdetails.json")
     public String Handler() {
 		return getMatchesDetails();
     }
-	@GetMapping("/getSeriesDetails")
+	@GetMapping("/getSeriesDetails.json")
     public String SeriesHandler() {
 		return getSeriesDetails();
     }
--- src/main/java/com/example/CricketAPI/weatherReportController.java
+++ src/main/java/com/example/CricketAPI/weatherReportController.java
@@ -1,21 +1,13 @@
 package com.example.CricketAPI;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
+
 public class weatherReportController extends weatherUtil{
 	
-	 private static final String[] cities = {"Bangalore", "Delhi", "kolkata", "Mumbai", "Hyderabad"};
-
-	    @GetMapping("/getweatherreport.json")
-	    public String getWeather() {
-	        StringBuilder result = new StringBuilder();
-	        for (String city : cities) {
-	            String weatherReport = getWeatherReport(city);
-	            result.append(weatherReport).append(System.lineSeparator());
-	        }
-	        return result.toString();
-	    }
+	@GetMapping("/getSeriesDetails")
+    public String weatherReport() {
+		return "test";
+    }
+	
 	
 }
- 
